294365c8cfcde334397c997d09cc647c84f58581
Joe Polny <joepolny@gmail.com>
2026-08-08 19:54:24 -0400

    sudo ssh agent auth

--- build-template.sh
+++ build-template.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -x
-limactl stop --force dev-template
-limactl delete dev-template
+set -ex
+
 limactl create -y dev-template.yaml
 limactl start dev-template
 
 limactl shell dev-template -- sh -c "rm -rf ~/.local/share/chezmoi"
 limactl shell dev-template -- sh -c "git clone --branch chezmoi https://github.com/joe-p/dotfiles.git ~/.local/share/chezmoi"
 ghtkn exec -- limactl shell dev-template -- sh -c "cd ~/.local/share/chezmoi/dot_setup && MISE_GITHUB_TOKEN=$GITHUB_TOKEN bash setup-ubuntu.sh"
+limactl shell dev-template -- sudo bash .setup/setup-sudo-ssh-agent-auth.sh
 
